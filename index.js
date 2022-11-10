821a337c5d6c1c2fd2415fac2424cd102c0c9b8e
codethi <docencia.thiago@gmail.com>
2022-11-10 16:55:47 -0300

    feat: create route post herois

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ try {
 const db = mongoClient.db("marvel"); // Se o banco existir ele ENTRA, se não, cria e entra
 const collectionHerois = db.collection("herois"); // Const global para criar e/ou entrar em uma coleção
 
+// ROTAS
+app.post("/herois", async (req, res) => {
+  const { nome, idade } = req.body;
+  if (!nome || !idade) {
+    res.sendStatus(400);
+    return;
+  }
+
+  try {
+    await collectionHerois.insertOne({ nome, idade });
+    res.sendStatus(201);
+    /* 
+    Quero receber a resposta estranha no insertOne:
+        const response = await collectionHerois.insertOne({nome, idade})
+        console.log(response)
+    */
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
 
 
 app.listen(process.env.PORT, () =>
